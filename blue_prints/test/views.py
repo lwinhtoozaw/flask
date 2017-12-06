d6f33d14f1c30473a2504103d9e9831a0cc6e620
lwin htoo zaw <mugenphi@gmail.com>
2017-12-06 14:39:57 +0630

    Change

--- blue_prints/test/views.py
+++ blue_prints/test/views.py
@@ -1,14 +1,20 @@
 from flask import Blueprint, render_template, session, request
 
-from peewee import *
+# Models
+from ..model_1 import *
 
 test = Blueprint('test', __name__)
 
-db = PostgresqlDatabase(
-    'hermit',
-    user = 'postgres',
-    password = 'qwerasdf',
-)
+@test.before_request
+def before_request():
+    # Create db if needed and connect
+    initialize_db()
+
+# This hook ensures that the connection is closed when we've finished processing the request.
+@test.teardown_request
+def _db_close(exc):
+    if not db.is_closed():
+        db.close()
 
 # Views
 @test.route('/')
