a80dad117a80167854a8a6f51e3a27be00302f7d
lwin htoo zaw <mugenphi@gmail.com>
2017-12-06 14:27:48 +0630

    change

--- blue_prints/test/views.py
+++ blue_prints/test/views.py
@@ -1,6 +1,14 @@
 from flask import Blueprint, render_template, session, request
 
+from peewee import *
+
 test = Blueprint('test', __name__)
+
+db = PostgresqlDatabase(
+    'hermit',
+    user = 'postgres',
+    password = 'qwerasdf',
+)
 
 # Views
 @test.route('/')
