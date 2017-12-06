088facef56a8cb3c23252593fef0776a812a813e
lwin htoo zaw <mugenphi@gmail.com>
2017-12-06 15:59:41 +0630

    change

--- blue_prints/test/views.py
+++ blue_prints/test/views.py
@@ -1,22 +1,11 @@
 from flask import Blueprint, render_template, session, request
+import redis
 
-# Models
-from ..model_1 import *
-
+r = redis.StrictRedis(host = 'localhost', port = 6379, db = 0)
 test = Blueprint('test', __name__)
-
-@test.before_request
-def before_request():
-    # Create db if needed and connect
-    initialize_db()
-
-# This hook ensures that the connection is closed when we've finished processing the request.
-@test.teardown_request
-def _db_close(exc):
-    if not db.is_closed():
-        db.close()
 
 # Views
 @test.route('/')
 def index():
-    return 'hah haha ha'
+    text = r.get('user:id')
+    return str(text)
